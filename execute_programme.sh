baacbfaa8d1b5c7f166329eaa85e2527525e57e3
gulshan kumar <gulhan@123>
2020-04-13 15:33:43 +0530

    Added .gitignore and execute_programme.sh files

---
+++ execute_programme.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+echo "compiling..."
+
+mkdir -p bin
+rm -rf ./*.o
+
+excecutable=$1
+
+gcc -c ./*.c
+gcc -o $excecutable ./*.o
+
+echo "compiling done"
+echo "executing..."
+
+mv $excecutable ./bin
+./bin/$excecutable
+
+rm -rf ./*.o

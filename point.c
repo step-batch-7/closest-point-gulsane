d5e63af5af6006f27b694cec3c831355e179e1d2
gulshan kumar <gulhan@123>
2020-04-13 15:53:01 +0530

    Added get_closet_food in point.c

--- point.c
+++ point.c
@@ -5,3 +5,16 @@ float find_distance(Point source, Point target)
 {
   return hypotf((source.x - target.x), (source.y - target.y));
 }
+
+void get_closest_food(Point food_points[], int points_length, Point current_location, Point *closest_food_location)
+{
+  *closest_food_location = food_points[0];
+
+  for (int index = 0; index < points_length; index++)
+  {
+    if (find_distance(current_location, *closest_food_location) > find_distance(current_location, food_points[index]))
+    {
+      *closest_food_location = food_points[index];
+    }
+  }
+}

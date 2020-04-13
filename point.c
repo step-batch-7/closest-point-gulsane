9f6afc1d9360d0bb7f487da67d18ec45795050d8
gulshan kumar <gulhan@123>
2020-04-13 17:32:06 +0530

    Refactored get_closest_food
    calculating current closest distance outside of for loop

--- point.c
+++ point.c
@@ -9,12 +9,14 @@ float find_distance(Point source, Point target)
 void get_closest_food(Point food_points[], int points_length, Point current_location, Point *closest_food_location)
 {
   *closest_food_location = food_points[0];
+  float current_closest_distance = find_distance(current_location, *closest_food_location);
 
   for (int index = 0; index < points_length; index++)
   {
-    if (find_distance(current_location, *closest_food_location) > find_distance(current_location, food_points[index]))
+    if (current_closest_distance > find_distance(current_location, food_points[index]))
     {
       *closest_food_location = food_points[index];
+      current_closest_distance = find_distance(current_location, *closest_food_location);
     }
   }
 }
